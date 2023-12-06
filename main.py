12543cd996ba87165efc15d5343d68341ee55a76
Zack Dowd <zdowd2796@gmail.com>
2023-12-06 14:29:09 -0700

    words to learn added to separate csv

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from tkinter import *
 import pandas
 import random
 
-data = pandas.read_csv("./data/french_words.csv")
-to_learn = data.to_dict(orient="records")
+to_learn = {}
 current_card = {}
+
+try:
+    data = pandas.read_csv("./data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("./data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 def next_card():
@@ -24,6 +31,13 @@ def flip_card():
     canvas.itemconfig(word_text, text=current_card["English"], fill="white")
 
 
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -38,13 +52,13 @@ title_text = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 word_text = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
-wrong_image = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card, )
-wrong_button.grid(row=1, column=0)
+x_image = PhotoImage(file="./images/wrong.png")
+x_button = Button(image=x_image, highlightthickness=0, command=next_card)
+x_button.grid(row=1, column=0)
 
-right_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, command=next_card)
-right_button.grid(row=1, column=1)
+check_image = PhotoImage(file="./images/right.png")
+check_button = Button(image=check_image, highlightthickness=0, command=is_known)
+check_button.grid(row=1, column=1)
 
 next_card()
 
